60920e8c25611e8e94f42f9a9331273f75700bf8
eduardocoraca <57689354+eduardocoraca@users.noreply.github.com>
2020-10-13 17:14:30 -0300

    Add files via upload

--- hmm/hmm_tools.py
+++ hmm/hmm_tools.py
@@ -2,6 +2,19 @@ import scipy.special
 from statsmodels.tsa.ar_model import AR
 import numpy as np
 
+def split_signal(signal, window_size, overlap):
+  # window_size: length of window
+  # overlap: % of overlap
+  signal = signal.reshape(-1)
+  X = []
+  o = round(overlap*window_size)
+  N = len(signal)
+  for k in range(0,N-window_size, window_size-o):
+    X.append(signal[k:k+window_size])
+
+  return (np.array(X))
+
+
 class hmm_ar():
     def __init__(self, hmm_list):
         self.hmm_list = hmm_list

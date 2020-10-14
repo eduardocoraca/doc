6a2bc276dae56a740b984b74272cef777e97d08b
eduardocoraca <57689354+eduardocoraca@users.noreply.github.com>
2020-10-14 09:33:14 -0300

    Update hmm_tools.py

--- hmm/hmm_tools.py
+++ hmm/hmm_tools.py
@@ -1,46 +1,40 @@
-import scipy.special
-from statsmodels.tsa.ar_model import AR
-import numpy as np
-from hmmlearn.hmm import *
-
-def split_signal(signal, window_size, overlap):
-  # window_size: length of window
-  # overlap: % of overlap
-  signal = signal.reshape(-1)
-  X = []
-  o = round(overlap*window_size)
-  N = len(signal)
-  for k in range(0,N-window_size, window_size-o):
-    X.append(signal[k:k+window_size])
-
-  return (np.array(X))
-
 class HMM_AR():
     def __init__(self, M, N):
         self.N = N
         self.M = M
 
-    def fit(self, X_train, num_states, num_mix):
+    def fit(self, X_train, num_states, num_mix, window_size, overlap, intervals=3):
         # X_train: list of time signals. Each component corresponds to a HMM
         assert len(X_train)==self.N
         self.models = []
+
         for k in range(self.N):
+            print('Training HMM',k, '...')
+            ar = self.get_ar(X_train[k])
+            x_fit = np.zeros((0,self.M))
+            len_fit = []
+            D = ar.shape[0]//intervals
+            for d in range(intervals):
+              x_fit = np.concatenate((x_fit,ar[d*D:(d+1)*D,:]), axis=0)
+              len_fit.append(D)
             hmm = GMMHMM(n_components=num_states, n_mix=num_mix, n_iter=100, verbose=False, init_params='smcw', tol=1e-3)
-            hmm.fit(X_train[k])
+            hmm.fit(x_fit, len_fit)
             self.models.append(hmm)
+            del hmm
 
     def predict(self, X):
         # X: array of shape [num_samples, windowed_time_signal]
         # M: number of AR coefficients
         # ar: array of shape [num_samples, M]
+        print('Extracting AR coefficients ...')
         ar = self.get_ar(X)
-
         num_samples = X.shape[0]
         predictions = np.zeros((num_samples, self.N))
 
+        print('Calculating probabilities ...')
         for i in range(num_samples):
             for j in range(self.N):
-                predictions[i,j] = self.models[j].score(X[i,:].reshape(-1,1))
+                predictions[i,j] = self.models[j].score(ar[i,:].reshape(1,-1))
             predictions[i,:] = scipy.special.softmax(predictions[i,:])
         return predictions
 
@@ -51,5 +45,3 @@ class HMM_AR():
             fitmod = mod.fit(self.M-1)
             a.append(fitmod.params)
         return np.array(a)
-
-        
